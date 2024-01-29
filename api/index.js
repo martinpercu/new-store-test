384ffd71b4f21deb8a9eafa2e72b0629bf2c9f08
Martin Mendez <martinpercu@gmail.com>
2024-01-29 13:18:38 -0300

    changes for vercel deploy

--- api/index.js
+++ api/index.js
@@ -10,22 +10,22 @@ const port = 3000;
 app.use(express.json());
 
 const whitelist = ['http://localhost/8080', 'http://localhost/3000', 'https://mysuperapp.com'];
-const options = = {
-  origin: (origin, callbacks) => {
-    if (whitelist.includes(origin)) {
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
       callback(null, true);
     } else {
       callback(new Error('not allow!! check cors issue'));
     }
   }
 }
-app.use(cors())
+app.use(cors(options))
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Hello in my serve Express');
 });
 
-app.get('/new-path', (req, res) => {
+app.get('/api/new-path', (req, res) => {
   res.send('Hello I\'m a new pathjpress');
 });
 
