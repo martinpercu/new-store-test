2aa2a319b482f6fbb67c12ec6c9cc10d72682954
Martin Mendez <martinpercu@gmail.com>
2024-01-30 00:24:39 -0300

    start implement node-postgres

--- api/routes/users.router.js
+++ api/routes/users.router.js
@@ -1,27 +1,75 @@
 const express = require('express');
-// const { faker } = require('@faker-js/faker');
+
+
+const UserService = require('./../services/user.service');
+const validatorHandler = require('./../middlewares/validator.handler');
+const { updateUserSchema, createUserSchema, getUserSchema } = require('./../schemas/user.schema');
 
 const router = express.Router();
+const service = new UserService();
 
-router.get('/', (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json({
-       limit,
-       offset
-    })
-  } else {
-    res.send('there is not parameters')
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await service.find();
+    res.json(users);
+  } catch (error) {
+    next(error);
   }
-})
+});
 
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({
-    id,
-    userName:"Juancito",
-    email: "qsdfsdf@sdf.com"
-  })
-})
+router.get('/:id',
+  validatorHandler(getUserSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const users = await service.findOne(id);
+      res.json(users);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post('/',
+  validatorHandler(createUserSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const newCategory = await service.create(body);
+      res.status(201).json(newCategory);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.patch('/:id',
+  validatorHandler(getUserSchema, 'params'),
+  validatorHandler(updateUserSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const users = await service.update(id, body);
+      res.json(users);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.delete('/:id',
+  validatorHandler(getUserSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      await service.delete(id);
+      res.status(201).json({id});
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
 
 module.exports = router;
